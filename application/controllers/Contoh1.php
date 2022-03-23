d675852afb1c64f47ff484d67a6eb781c3b4f328
AripBudiman12 <arip24.ab@gmail.com>
2022-03-23 17:48:49 +0700

    menambah isi Contoh1.php

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -6,6 +6,6 @@ class Contoh1 extends CI_Controller
         echo "<h1>Perkenalkan</h1>";
         echo "Nama saya Arip Budiman
               saya tinggal di daerah pegadungan
-              olah raga yang saya sukai adalah catur";
+              olah raga yang saya sukai adalah Bulutangkis";
     }
 }
